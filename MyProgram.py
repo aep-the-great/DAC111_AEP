f7e9fcbd8c1205ae244953d3c615958304ca2c74
apflughoeft <pflughoeftalex@outlook.com>
2021-04-24 18:44:59 -0500

    Added the main function and the main function call

--- MyProgram.py
+++ MyProgram.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 24 17:50:38 2021
-
+Created on Sat Apr 23 17:50:38 2021
 @author: pflug
 """
 
 
+def main():
+    print('Hello World')
+    
+main()
