2fa56a96f8aa1aa8c773e1abb4460b52b72908af
apflughoeft <pflughoeftalex@outlook.com>
2021-04-26 19:37:13 -0500

    Added code to get the top 5 rows from the flights.db

--- MyProgram.py
+++ MyProgram.py
@@ -4,9 +4,22 @@ Created on Sat Apr 23 17:50:38 2021
 @author: pflug
 """
 
+import sqlite3 as sl
+
+FLIGHTS_DB = "C:/src/DAC_111/flights.db"
 
 def main():
-    print('Hello World')
-    print('Hi')
+    connection = sl.connect(FLIGHTS_DB)
+    cursor = connection.cursor()
+    
+    query = "select * from airlines limit 5;"
+    cursor.execute(query)
+    
+    results = cursor.fetchall()
+    print(results)
+    
+    cursor.close()
+    connection.close()
+    
     
 main()
